170dd24488b732ea010cadc80fc3b188b9001072
barbiconte <contebarbi@gmail.com>
2024-02-27 16:12:01 -0300

    cambios en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar  from './components/Navbar/Navbar';
 import Login from './components/Login/Login';
-import Header from './components/Header/Header';
 import Form from './components/Form/Form';
 import Footer from './components/Footer/Footer';
 import "bootstrap/dist/css/bootstrap.min.css";
