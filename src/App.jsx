cafd102675130057d336b5e6f5e809e660fc7a22
Fer <fer.sauretti@gmail.com>
2024-02-27 08:21:08 -0300

    creacion de form

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,13 @@
 import './App.css'
-import { useState } from 'react';
-//Modulos de firebase
-
-import {FirebaseApp} from './Firebase/config'
-import {getAuth, onAuthStateChanged} from 'firebase/auth';
-
-
-//importación de componentes
-
-
-import Login from './screens/Login';
-import Home from './screens/Home';
-
-
-//Funciones y variables 
-
-const auth = getAuth (FirebaseApp);
-
-
+import Form from './components/Form/Form';
 
 function App() {
 
- const [User,setUser] = useState(null)
-  onAuthStateChanged(auth, (userFirebase) => {
-    if (userFirebase) {
-      setUser(userFirebase)
-    }
-    else
-    {
-      setUser(null)
-    }
-  })
 
-  return (
-      <div>
-        {User ? <Home correoUser = {User.email}/> : <Login/> };
-    
+return (
+<Form/>
 
-      
-       
-      </div>
-      
-  )
+);
+}
 
-  }
-export default App;
+export default App
