07bdbab26da97bd3ee67f7ae98d130cb933428c8
Fer <fer.sauretti@gmail.com>
2024-03-12 02:09:26 -0300

    Form MUI

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,35 +1,81 @@
 import React from 'react';
-import { Container, Form, FormGroup, Label, Input, Button } from 'reactstrap';
-import './Form.css'
+import { Container, TextField, Button, Grid } from '@mui/material';
+
 const FormMain = () => {
     return (
-        <Container>
-            <Form>
-                <FormGroup>
-                    <Label for="nombre">Nombre</Label>
-                    <Input type="text" name="nombre" id="nombre" placeholder="Ingrese su nombre" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="apellido">Apellido</Label>
-                    <Input type="text" name="apellido" id="apellido" placeholder="Ingrese su apellido" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="telefono">Teléfono</Label>
-                    <Input type="tel" name="telefono" id="telefono" placeholder="Ingrese su teléfono" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="email">Email</Label>
-                    <Input type="email" name="email" id="email" placeholder="Ingrese su email" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="mensaje">Describe tu problema</Label>
-                    <Input type="textarea" name="mensaje" id="mensaje" placeholder="Escriba su mensaje aquí" />
-                </FormGroup>
-                <Button color="primary">Enviar</Button>
-            </Form>
+        <Container maxWidth="sm">
+            <form>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            label="Nombre"
+                            variant="outlined"
+                            name="nombre"
+                            id="nombre"
+                            placeholder="Ingrese su nombre"
+                            fullWidth
+                            margin="normal"
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            label="Apellido"
+                            variant="outlined"
+                            name="apellido"
+                            id="apellido"
+                            placeholder="Ingrese su apellido"
+                            fullWidth
+                            margin="normal"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            label="Teléfono"
+                            variant="outlined"
+                            name="telefono"
+                            id="telefono"
+                            type="tel"
+                            placeholder="Ingrese su teléfono"
+                            fullWidth
+                            margin="normal"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            label="Email"
+                            variant="outlined"
+                            name="email"
+                            id="email"
+                            type="email"
+                            placeholder="Ingrese su email"
+                            fullWidth
+                            margin="normal"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            label="Describe tu problema"
+                            variant="outlined"
+                            name="mensaje"
+                            id="mensaje"
+                            multiline
+                            rows={4}
+                            placeholder="Escriba su mensaje aquí"
+                            fullWidth
+                            margin="normal"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Button variant="contained" color="primary" fullWidth>
+                            Enviar
+                        </Button>
+                    </Grid>
+                </Grid>
+            </form>
         </Container>
     );
 }
 
 export default FormMain;
+
 
